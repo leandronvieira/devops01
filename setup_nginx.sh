857b88ee12064cdd9043761126518a128a910e8a
leandronvieira <leandronvieira@gmail.com>
2025-06-28 18:19:01 -0300

    v7

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# Verifica se o Nginx já está instalado
 if ! command -v nginx >/dev/null 2>&1; then
-  echo "Instalando Nginx..."
-  yum update -y
-  amazon-linux-extras enable nginx1
-  yum install -y nginx
+  echo "Nginx não encontrado. Instalando com yum..."
+  sudo yum update -y
+
+  # Habilita o repositório do nginx (disponível via amazon-linux-extras)
+  sudo amazon-linux-extras enable nginx1
+  sudo yum clean metadata
+  sudo yum install -y nginx
 else
-  echo "Nginx já está instalado"
+  echo "Nginx já está instalado. Nenhuma ação necessária."
 fi
