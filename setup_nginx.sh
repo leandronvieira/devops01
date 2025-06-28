f1a2a939ff68acdfdfd4a9c8e751d6a36f35cf4b
leandronvieira <leandronvieira@gmail.com>
2025-06-28 17:11:03 -0300

    v3

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -1,12 +1,10 @@
-sudo amazon-linux-extras install -y nginx1
-sudo cp /var/www/html/index.html /usr/share/nginx/html/index.html
 #!/bin/bash
 
 # Verifica se nginx está instalado e instala apenas se necessário
 if ! command -v nginx >/dev/null 2>&1; then
   echo "Nginx não encontrado. Instalando..."
-  apt-get update -y
-  apt-get install nginx -y
+  dnf install -y nginx
 else
   echo "Nginx já está instalado. Nenhuma ação necessária."
 fi
+
